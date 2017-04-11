b93812645abd9d00849f26b237dbe3ef60b1412d
unknown <>
2017-04-11 09:26:18 +0900

    테스트

--- src/components/Base/Header.js
+++ src/components/Base/Header.js
@@ -2,12 +2,11 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Icon } from 'semantic-ui-react';
 
-
 const Header = ({onClick}) => {
     return (
         <div>
             <div className="header-warper"> 
-                <div className="header">
+                <div className="header">    
                     <div className="sidebar-button"><Icon name="sidebar" /></div>
                     <div className="logo">blog</div>
                     <div className="login-btn-area" onClick={onClick} ><div className="auth-button"><Icon name="user" />로그인</div></div>
