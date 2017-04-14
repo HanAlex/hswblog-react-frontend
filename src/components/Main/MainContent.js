838e49f9b7a69be9cfe208a5ec16ee7170a24e2e
unknown <>
2017-04-14 11:14:42 +0900

    UI 중간 업데이트

--- src/components/Main/MainContent.js
+++ src/components/Main/MainContent.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import {Icon} from 'semantic-ui-react';
 
 const MainContent = () => {
     return (
         <div className="center-area">
             <div className="write-area">
-                <div className="write-header">글쓰기 헤더</div>
-                <div className="write-content">글쓰기 영역</div>
+                <div className="write-header">
+                    <div className="btn-area">
+                    <a className="write-btn"><Icon name="write" /><span>글쓰기</span></a>
+                    <a className="write-btn"><Icon name="write" /><span>글쓰기</span></a>
+                    <a className="write-btn"><Icon name="write" /><span>글쓰기</span></a>
+                    </div>
+                </div>
+                <div className="write-content">
+                    <p className="desctiption">글쓰기 영역</p>
+                </div>
             </div>
-            <div className="content-list">
+            <div className="content-area">
                 <div className="content-item"></div>
             </div>
         </div>
